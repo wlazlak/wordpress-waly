102b0a5dcfdacea98efb31b3a611ea82b5d565d3
Jan Blasko <honza@lucien144.net>
2015-02-14 23:23:52 +0100

    Return variable

--- src/Waly/post.php
+++ src/Waly/post.php
@@ -6,6 +6,34 @@ use Lucien144\Wordpress\Waly\Author;
 
 class Post {
 
+	/*
+	   [ID]                    => (integer)
+	   [post_author]           => (integer)
+	   [post_date]             => (YYYY-MM-DD HH:MM:SS)
+	   [post_date_gmt]         => (YYYY-MM-DD HH:MM:SS)
+	   [post_content]          => (all post content is in here)
+	   [post_title]            => (Post Title Here)
+	   [post_excerpt]          => (Post Excerpt)
+	   [post_status]           => (? | publish)
+	   [comment_status]        => (? | closed)
+	   [ping_status]           => (? | closed)
+	   [post_password]         => (blank if not specified)
+	   [post_name]             => (slug-is-here)
+	   [to_ping]               => (?)
+	   [pinged]                => (?)
+	   [post_modified]         => (YYYY-MM-DD HH:MM:SS)
+	   [post_modified_gmt]     => (YYYY-MM-DD HH:MM:SS)
+	   [post_content_filtered] => (?)
+	   [post_parent]           => (integer)
+	   [guid]                  => (a unique identifier that is not necessarily the URL to the Page)
+	   [menu_order]            => (integer)
+	   [post_type]             => (? | page)
+	   [post_mime_type]        => ()?)
+	   [comment_count]         => (integer)
+	   [ancestors]             => (object|array)
+	   [filter]                => (? | raw)
+	*/
+
 	private $post;
 
 	public function __construct(\WP_Post $post)
