d62b38dba1d68665ea6d72c43d78a7490ccb5071
Jan Blasko <honza@lucien144.net>
2015-02-15 00:48:49 +0100

    require_once -> require for debug

--- src/waly.php
+++ src/waly.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/Waly/waly.php';
-require_once __DIR__ . '/Waly/post.php';
-require_once __DIR__ . '/Waly/category.php';
-require_once __DIR__ . '/Waly/author.php';
+require __DIR__ . '/Waly/waly.php';
+require __DIR__ . '/Waly/post.php';
+require __DIR__ . '/Waly/category.php';
+require __DIR__ . '/Waly/author.php';
